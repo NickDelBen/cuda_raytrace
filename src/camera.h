ec73ea826e43c631b8272aaed31d4af17db342e7
Nick DelBen <nick.delben@gmail.com>
2017-04-26 12:23:09 -0400

    fix raytracer file

--- src/camera.h
+++ src/camera.h
@@ -33,21 +33,7 @@ typedef struct camera_t {
 ******************************/
 camera_t* Camera_read (FILE* file);
 
-/******************************
-* Creates the rays from a camera on the device
-* @param camera Camera on device to create rays for
-* @param rays   Pointer to memory allocated on device for rays
-******************************/
-void Camera_createRays (camera_t* camera, line_t* rays, unsigned int blocks, unsigned int threads);
 
-/******************************
-* Kernel for creating the rays from a camera on the device
-* @param camera Camera on device to create rays for
-* @param rays   Pointer to memory allocated on device for rays
-* @param to_do  Number of rays to be created per thread
-* @param extra  How many sections need one additional ray
-******************************/
-__global__ void Camera_createRays_k (camera_t* camera, line_t* rays, unsigned int to_do, unsigned int extra);
 
 /******************************
 * Copies the specified camera to the device
--- src/raytracer.h
+++ src/raytracer.h
@@ -4,5 +4,20 @@
 #ifndef _h_raytracer
 #define _h_raytracer
 
+/******************************
+* Creates the rays from a camera on the device
+* @param camera Camera on device to create rays for
+* @param rays   Pointer to memory allocated on device for rays
+******************************/
+void Camera_createRays (camera_t* camera, line_t* rays, unsigned int blocks, unsigned int threads);
+
+/******************************
+* Kernel for creating the rays from a camera on the device
+* @param camera Camera on device to create rays for
+* @param rays   Pointer to memory allocated on device for rays
+* @param to_do  Number of rays to be created per thread
+* @param extra  How many sections need one additional ray
+******************************/
+__global__ void Camera_createRays_k (camera_t* camera, line_t* rays, unsigned int to_do, unsigned int extra);
 
 #endif
